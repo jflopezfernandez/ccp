7081a4c2e0ffa91df349c5d6f8b9be95a9ce9239
JFLF <jflopezfernandez@gmail.com>
2017-06-04 16:26:39 -0400

    Structuring...

---
+++ src/includes/Main.hpp
@@ -0,0 +1,22 @@
+#ifndef _MAIN_HPP
+#define _MAIN_HPP
+
+
+/** C++ Standard Library */
+#include <iostream>
+
+
+/** C Standard Library Headers */
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/** Boost Headers */
+
+
+
+/** Application Headers */
+
+
+
+#endif /* _MAIN_HPP */
